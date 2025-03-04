31de1f46c5988045e9e3cef905432e2048788819
arnabnandikgp <arnabnandi2002@gmail.com>
2025-03-04 08:52:32 +0530

    added json validations and implemented custom json decoding
    Signed-off-by: arnabnandikgp <arnabnandi2002@gmail.com>

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,3 +1,4 @@
+// all the handlers for the api are defined here
 package main
 
 import (
@@ -6,18 +7,34 @@ import (
 	"time"
 
 	"github.com/arnabnandikgp/greenlight/internal/data"
+	"github.com/arnabnandikgp/greenlight/internal/validator"
 )
 
 func (app *application) createMoviesHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	// Initialize a new Validator.
+	v := validator.New()
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,8 +1,10 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
@@ -12,4 +14,23 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 
 	qJSONValue := strconv.Quote(jsonvalue)
 	return []byte(qJSONValue), nil
+}
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime error")
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(i)
+	return nil
 }
